7d3b25150be1a9b920591bdbd72fc596c1b9b856
Ross Reisman <Ross.Reisman@gmail.com>
2023-03-01 12:45:45 -0500

    Update videogame.sql

--- SQL/videogame.sql
+++ SQL/videogame.sql
@@ -9,6 +9,118 @@ Distinct, Count, Group By, Having, Order By, Count, Avg, Alias, Limit.
 ------------------------------------------------------------------
 /*
 
+First let's examine the dataset
+
+see how many rows and columns are in this datasets
+
+*/
+
+SELECT COUNT(*) FROM vgsales
+
+SELECT COUNT (column_name) AS Number
+FROM information_schema.columns
+WHERE table_name='vgsales'
+
+Checking column data types vvv
+
+SELECT column_name, DATA_TYPE
+FROM information_schema.columns
+WHERE table_name='vgsales'
+
+
+Generating summary statistics vvv
+
+SELECT 'total',
+	sum(na_sales) as na_sales,
+	sum(eu_sales) as eu_sales,
+	sum(jp_sales) as jp_sales,
+	sum(other_sales) as ot_sales
+from vgsales
+union
+select 'average',
+	round(avg(na_sales),2) as na_sales,
+	round(avg(eu_sales),2) as eu_sales,
+	round(avg(jp_sales),2) as jp_sales,
+	round(avg(other_sales),2) as ot_sales
+from vgsales
+union
+select 'min',
+	min(na_sales) as na_sales,
+	min(eu_sales) as eu_sales,
+	min(jp_sales) as jp_sales,
+	min(other_sales) as ot_sales
+from vgsales
+union
+select 'max',
+	max(na_sales) as na_sales,
+	max(eu_sales) as eu_sales,
+	max(jp_sales) as jp_sales,
+	max(other_sales) as ot_sales
+from vgsales
+
+IQR
+
+SELECT
+	PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY na_sales) -
+	PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY na_sales) as NA_IQR,
+	PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY eu_sales) -
+	PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY eu_sales) as EU_IQR,
+	PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY jp_sales) -
+	PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY jp_sales) as JP_IQR,
+	PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY other_sales) -
+	PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY other_sales) as OT_IQR
+FROM vgsales
+
+Quartiles
+
+SELECT 'min',
+	PERCENTILE_CONT(0) WITHIN GROUP (ORDER BY na_sales) as NA_sales,
+	PERCENTILE_CONT(0) WITHIN GROUP (ORDER BY eu_sales) as EU_sales,
+	PERCENTILE_CONT(0) WITHIN GROUP (ORDER BY jp_sales) as JP_sales,
+	PERCENTILE_CONT(0) WITHIN GROUP (ORDER BY other_sales) as OT_sales
+FROM vgsales
+UNION
+SELECT '25%',
+	PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY na_sales) as NA_25,
+	PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY eu_sales) as EU_25,
+	PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY jp_sales) as JP_25,
+	PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY other_sales) as OT_25
+FROM vgsales
+UNION
+SELECT '50%',
+	PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY na_sales) as NA_50,
+	PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY eu_sales) as EU_50,
+	PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY jp_sales) as JP_50,
+	PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY other_sales) as OT_50
+FROM vgsales
+UNION
+SELECT '75%',
+	PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY na_sales) as NA_75,
+	PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY eu_sales) as EU_75,
+	PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY jp_sales) as JP_75,
+	PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY other_sales) as OT_75
+FROM vgsales
+UNION
+SELECT 'max',
+	PERCENTILE_CONT(1) WITHIN GROUP (ORDER BY na_sales) as NA_100,
+	PERCENTILE_CONT(1) WITHIN GROUP (ORDER BY eu_sales) as EU_100,
+	PERCENTILE_CONT(1) WITHIN GROUP (ORDER BY jp_sales) as JP_100,
+	PERCENTILE_CONT(1) WITHIN GROUP (ORDER BY other_sales) as OT_100
+FROM vgsales
+ORDER BY 2
+
+
+/*
+
+
+
+There are 16,598 rows and 11 columns.
+
+*/
+------------------------------------------------------------------
+
+/*
+
 First let's view a sample of our table.
 It's always good to test your query first using LIMIT.
 
