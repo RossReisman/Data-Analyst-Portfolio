49efbd341c33741c4b1a05369d807a66f2313906
Ross Reisman <Ross.Reisman@gmail.com>
2023-07-03 14:35:17 -0400

    Update brazil.sql

--- SQL/brazil/brazil.sql
+++ SQL/brazil/brazil.sql
@@ -21,7 +21,7 @@ CREATE TABLE orders (
 	order_approved_at TIMESTAMP,
 	order_delivered_carrier_date TIMESTAMP,
 	order_delivered_customer_date TIMESTAMP,
-	order_delivered_delivery_date TIMESTAMP
+	order_estimated_delivery_date TIMESTAMP
 )
 
 CREATE TABLE sellers (
@@ -84,7 +84,58 @@ CREATE TABLE products (
 	product_width_cm FLOAT
 )
 
-Step 2: Add Primary and Foreign Keys
+Step 2: Import Data (Using PSQL)
+
+/*
+Queries are written on multiple lines for ease of reading here
+*/
+\COPY public.categories(product_category_name, product_category_name_english)
+FROM '/Users/raws/Documents/GitHub/portfolio/SQL/brazil/product_category_name_translation.csv'
+DELIMITER ','
+CSV HEADER;
+
+\COPY public.sellers(seller_id, seller_zip_code_prefix, seller_city, seller_state)
+FROM '/Users/raws/Documents/GitHub/portfolio/SQL/brazil/olist_sellers_dataset.csv'
+DELIMITER ','
+CSV HEADER;
+
+\COPY public.customers(customer_id, customer_unique_id, customer_zip_code_prefix, customer_city, customer_state)
+FROM '/Users/raws/Documents/GitHub/portfolio/SQL/brazil/olist_customers_dataset.csv'
+DELIMITER ','
+CSV HEADER;
+
+\COPY public.products(product_id, product_category_name, product_name_length, product_description_length, product_photos_qty, product_weight_g, product_length_cm, product_height_cm, product_width_cm)
+FROM '/Users/raws/Documents/GitHub/portfolio/SQL/brazil/olist_products_dataset.csv'
+DELIMITER ','
+CSV HEADER;
+
+\COPY public.payments(order_id, payment_sequential, payment_type, payment_installments, payment_value)
+FROM '/Users/raws/Documents/GitHub/portfolio/SQL/brazil/olist_order_payments_dataset.csv'
+DELIMITER ','
+CSV HEADER;
+
+\COPY public.geolocation(geolocation_zip_code_prefix, geolocation_lat, geolocation_lng, geolocation_city, geolocation_state)
+FROM '/Users/raws/Documents/GitHub/portfolio/SQL/brazil/olist_geolocation_dataset.csv'
+DELIMITER ','
+CSV HEADER;
+
+\COPY public.items(order_id, order_item_id, product_id, seller_id, shipping_limit_date, price, freight_value)
+FROM '/Users/raws/Documents/GitHub/portfolio/SQL/brazil/olist_order_items_dataset.csv'
+DELIMITER ','
+CSV HEADER;
+
+\COPY public.reviews(review_id, order_id, review_score, review_comment_title, review_comment_message, review_creation_date, review_answer_timestamp)
+FROM '/Users/raws/Documents/GitHub/portfolio/SQL/brazil/olist_order_reviews_dataset.csv'
+DELIMITER ','
+CSV HEADER;
+
+\COPY public.orders(order_id, customer_id, order_status, order_purchase_timestamp, order_approved_at, order_delivered_carrier_date, order_delivered_customer_date, order_delivered_delivery_date)
+FROM '/Users/raws/Documents/GitHub/portfolio/SQL/brazil/olist_orders_dataset.csv'
+DELIMITER ','
+CSV HEADER;
+
+
+Step 3: Add Primary and Foreign Keys
 
 ALTER TABLE customers
 ADD PRIMARY KEY (customer_id)
@@ -144,23 +195,3 @@ FOREIGN KEY (seller_id)
 REFERENCES sellers (seller_id);
 
 ADD GEOLOCATION FK
-
-Step 3: Import Data (Using PSQL)
-
-/*
-Queries are written on multiple lines for ease of reading here
-*/
-\COPY public.categories(product_category_name, product_category_name_english)
-FROM '/Users/raws/Documents/GitHub/portfolio/SQL/brazil/product_category_name_translation.csv'
-DELIMITER ','
-CSV HEADER;
-
-\COPY public.sellers(seller_id, seller_zip_code_prefix, seller_city, seller_state)
-FROM '/Users/raws/Documents/GitHub/portfolio/SQL/brazil/olist_sellers_dataset.csv'
-DELIMITER ','
-CSV HEADER;
-
-\COPY public.customers(customer_id, customer_unique_id, customer_zip_code_prefix, customer_city, customer_state)
-FROM '/Users/raws/Documents/GitHub/portfolio/SQL/brazil/olist_customers_dataset.csv'
-DELIMITER ','
-CSV HEADER;
