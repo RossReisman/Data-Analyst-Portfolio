98993c8aa015b683a2f1247a911a884c7163c31e
Ross Reisman <Ross.Reisman@gmail.com>
2023-07-23 13:42:02 -0400

    Update brazil_EDA.sql

--- SQL/brazil/brazil_EDA.sql
+++ SQL/brazil/brazil_EDA.sql
@@ -16,16 +16,116 @@ FROM (SELECT DISTINCT seller_state
 FROM sellers) as distinct_state;
 
 SELECT COUNT(*)
-FROM (SELECT DISTINCT(INITCAP(seller_city) || ', ' || seller_state)
-AS seller_city_state
+FROM (SELECT DISTINCT seller_city_state
 FROM sellers)
-AS location_COUNT;
+AS distinct_city_state;
 
 /*
 There are 2246 unique zip code prefixes
 There are 611 unique seller cities
 There are 23 unique seller states
 There are 636 unique city states
+
 Discrepancy between unique cities and city/states
 may be due to multiple cities sharing a name
 */
+
+SELECT DISTINCT seller_city
+FROM sellers
+Group by 1
+order by 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+blank space baby
