14c95bd61138ebe7b5998952d5e00468e71d0103
Ross Reisman <Ross.Reisman@gmail.com>
2023-09-10 18:06:53 -0400

    Update player_data.sql

--- SQL/player_data/player_data.sql
+++ SQL/player_data/player_data.sql
@@ -4,6 +4,13 @@ Skills Used: CREATE, COPY, ALTER, UPDATE, SET, SELECT, WHERE, AND, OR, NOT, IS,
 NULL, INTERVAL, LIKE/NOT LIKE, MIN/MAX, DISTINCT, BETWEEN, GROUP BY, HAVING,
 ORDER BY, COUNT, AVG, ALIAS, LIMIT, UNION, FLOOR.
 
+/*
+- join the tables to find the total seconds between logins and logouts
+- answer "how much damage did each player take in total"
+- what was the average damage taken per player
+- what player died the most/least
+- what time of day do players die the most (lol not really something anyone would ask)
+*/
 
 Step 1: Create Tables
 
@@ -163,16 +170,63 @@ ORDER BY player_lifetime DESC
 "2f9c6e8d1b"	"2023-08-24 03:37:28"	"2023-08-26 01:28:54"	165086
 "b5a8c3f9e2"	"2023-08-25 09:23:42"	"2023-08-26 03:42:18"	65916
 
+-- How much damage did each player take in total?
 
+SELECT user_id as player, sum(damage_taken) as total_damage
+FROM damage
+group by 1
+order by 2 desc
 
+"9e1a3c7f5b"	68
+"8b0e7f3c9a"	64
+"d7f1e9b2a4"	62
+"4d6a2f8c1e"	62
+"c5d8a9f1e2"	54
+"f1d5a7e0c9"	17
+"b6f2d5a8c0"	12
+"a2e4f8c6b0"	7
+"e2b9d4a6f8"	6
+"3a5c7e9b1d"	3
 
+--what was the average damage taken per player
 
+SELECT user_id as player, round(avg(damage_taken),2) as average_damage
+FROM damage
+group by 1
+order by 2 desc
 
+"b6f2d5a8c0"	12.00
+"9e1a3c7f5b"	9.71
+"8b0e7f3c9a"	9.14
+"4d6a2f8c1e"	8.86
+"d7f1e9b2a4"	8.86
+"c5d8a9f1e2"	7.71
+"a2e4f8c6b0"	7.00
+"e2b9d4a6f8"	6.00
+"3a5c7e9b1d"	3.00
 
+--what player died the most/least? (3 damage = 1 death)
 
+alter table damage
+add num_deaths int;
+update damage
+set num_deaths = floor(damage_taken/3)
 
+select user_id as player, sum(num_deaths)
+from damage
+group by 1
+order by 2 desc
 
-
+"9e1a3c7f5b"	21
+"d7f1e9b2a4"	19
+"8b0e7f3c9a"	19
+"4d6a2f8c1e"	18
+"c5d8a9f1e2"	17
+"f1d5a7e0c9"	5
+"b6f2d5a8c0"	4
+"e2b9d4a6f8"	2
+"a2e4f8c6b0"	2
+"3a5c7e9b1d"	1
 
 
 
