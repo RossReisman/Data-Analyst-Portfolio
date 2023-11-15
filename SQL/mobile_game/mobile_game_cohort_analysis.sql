34a0413fbd0806627ac6d0e21481687f2bba0182
Ross Reisman <Ross.Reisman@gmail.com>
2023-11-15 06:53:44 -0500

    Update python and sql mobile game projects

--- SQL/mobile_game/mobile_game_cohort_analysis.sql
+++ SQL/mobile_game/mobile_game_cohort_analysis.sql
@@ -4,3 +4,21 @@ Data from <p><a href="https://www.kaggle.com/datasets/manchvictor/prediction-of-
 Skills Used: CREATE, COPY, ALTER, UPDATE, SET, SELECT, WHERE, EXTRACT, CTE,
 GROUP BY, HAVING, ORDER BY, COUNT, AVG, ALIAS, UNION, FLOOR, CAST, CASE.
 */
+
+CREATE TABLE users (
+  user_id TEXT
+  , level_id TEXT
+  , success INT
+  , duration DECIMAL
+  , reststep DECIMAL
+  , help INT
+  , time TIMESTAMP
+)
+
+CREATE TABLE levels (
+  level_id TEXT
+  , avg_duration DECIMAL
+  , avg_passrate DECIMAL
+  , avg_win_duration DECIMAL
+  , avg_retrytimes DECIMAL
+)
