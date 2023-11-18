f49b4e333e46b099890ba32a19ee315877ccd141
Ross Reisman <Ross.Reisman@gmail.com>
2023-11-18 09:45:42 -0500

    Update mobile_game_cohort_analysis.sql

--- SQL/mobile_game/mobile_game_cohort_analysis.sql
+++ SQL/mobile_game/mobile_game_cohort_analysis.sql
@@ -8,8 +8,8 @@ GROUP BY, HAVING, ORDER BY, COUNT, AVG, ALIAS, UNION, FLOOR, CAST, CASE.
 Step 1: Import
 
 CREATE TABLE users (
-  user_id TEXT
-  , level_id TEXT
+  user_id INT
+  , level_id INT
   , success INT
   , duration DECIMAL
   , reststep DECIMAL
@@ -30,10 +30,7 @@ Remember that PSQL commands are written on one line
 Written on multiple lines here for ease of reading
 */
 
-\COPY public.users(user_id, level_id, success, duration, reststep, help, time)
-FROM '/Users/raws/Downloads/mobile_game_data/level_seq.csv'
-DELIMITER E'\t'
-CSV HEADER;
+\COPY public.users(user_id, level_id, success, duration, reststep, help, time) FROM '/Users/raws/Downloads/mobile_game_data/level_seq.csv' DELIMITER E'\t' CSV HEADER;
 
 \COPY public.levels(avg_duration, avg_passrate, avg_win_duration, avg_retrytimes, level_id)
 FROM '/Users/raws/Downloads/mobile_game_data/level_meta.csv'
