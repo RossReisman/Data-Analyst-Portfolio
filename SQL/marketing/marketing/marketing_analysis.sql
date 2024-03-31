7f4a0362e32833eb0369812c430e480095c4f120
Ross Reisman <Ross.Reisman@gmail.com>
2024-03-31 17:44:28 -0400

    Update marketing_analysis.sql

--- SQL/marketing/marketing/marketing_analysis.sql
+++ SQL/marketing/marketing/marketing_analysis.sql
@@ -5,6 +5,7 @@ Which gender uses coupon codes more?
 Which location spends/coupons the most?
 Which month had the highest ROAS?
 Which customers spends/coupons the most?
+Breakdown of products by revenue/coupon use
 
 
 
