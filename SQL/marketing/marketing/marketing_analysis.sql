5cfccf27d96e36ffbb05d6a2ff21f40c53c40778
Ross Reisman <Ross.Reisman@gmail.com>
2024-03-31 14:37:36 -0400

    Update marketing_analysis.sql

---
+++ SQL/marketing/marketing/marketing_analysis.sql
@@ -0,0 +1,11 @@
+/*
+Why are some delivery charges so steep?
+Which gender spends more?
+Which gender uses coupon codes more?
+Which location spends/coupons the most?
+Which month had the highest ROAS?
+Which customers spends/coupons the most?
+
+
+
+*/
