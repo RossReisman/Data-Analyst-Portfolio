a946550fc33d658d0fa2724fec14555e8a289ec4
Ross Reisman <Ross.Reisman@gmail.com>
2024-04-07 17:18:08 -0400

    Update marketing_analysis.sql

--- SQL/marketing/marketing/marketing_analysis.sql
+++ SQL/marketing/marketing/marketing_analysis.sql
@@ -83,14 +83,116 @@ order by 1, 2
   "Waze"	                 "Not Used"	     89
   "Waze"	                 "Used"	         184
 
+1.5) Express percentages
 
-2) How much of each product category were coupons used?
+with calcs as (select
+  product_cat
+  , coupon_status
+  , count(coupon_status) as count
+  , sum(count(coupon_status)) OVER(PARTITION BY product_cat) AS total_count
+from sales
+group by 1,2
+order by 1, 2
+)
+select
+	product_cat
+	, coupon_status
+	, round(100 * count/total_count, 2) as pct
+from calcs
+
+"product_cat"	    "coupon_status"	    "pct"
+"Accessories"	      "Clicked"	        53.42
+"Accessories"	      "Not Used"	      13.68
+"Accessories"	       "Used"	          32.91
+"Android"	          "Clicked"	        53.49
+"Android"	          "Not Used"	      23.26
+"Android"	            "Used"	        23.26
+"Apparel"	          "Clicked" 	      50.88
+"Apparel"	          "Not Used"	      15.16
+"Apparel"	           "Used"	          33.96
+"Backpacks"	        "Clicked"	        52.81
+"Backpacks"	        "Not Used"	      16.85
+"Backpacks"	         "Used"	          30.34
+"Bags"	            "Clicked"	        51.65
+"Bags"	            "Not Used"	      15.14
+"Bags"	              "Used"	        33.21
+"Bottles"	          "Clicked"	        51.49
+"Bottles"	          "Not Used"	      17.91
+"Bottles"	            "Used"	         30.60
+"Drinkware"	        "Clicked"	          50.30
+"Drinkware"	      "Not Used"	         16.37
+"Drinkware"	           "Used"	          33.33
+"Fun"	              "Clicked"	          51.88
+"Fun"	            "Not Used"	         15.63
+"Fun"	                "Used"	         32.50
+"Gift Cards"	     "Clicked"	          50.94
+"Gift Cards"	     "Not Used"	         13.84
+"Gift Cards"	         "Used"	         35.22
+"Google"	         "Clicked"	          48.57
+"Google"	         "Not Used"	         23.81
+"Google"	             "Used"	         27.62
+"Headgear"	       "Clicked"	          52.01
+"Headgear"	       "Not Used"	         14.79
+"Headgear"	           "Used"	         33.20
+"Housewares"	     "Clicked"	          55.74
+"Housewares"	     "Not Used"	         14.75
+"Housewares"	         "Used"	         29.51
+"Lifestyle"	        "Clicked"	          49.45
+"Lifestyle"	      "Not Used"	         14.94
+"Lifestyle"	          "Used"	         35.61
+"More Bags"	        "Clicked"	          54.35
+"More Bags"	      "Not Used"	         6.52
+"More Bags"	          "Used"	         39.13
+"Nest"	           "Clicked"	          51.27
+"Nest"	           "Not Used"	         15.97
+"Nest"	               "Used"	         32.76
+"Nest-Canada"	      "Clicked"	          55.21
+"Nest-Canada"	     "Not Used"	          14.83
+"Nest-Canada"	        "Used"	         29.97
+"Nest-USA"	       "Clicked"	          50.96
+"Nest-USA"	       "Not Used"	         15.41
+"Nest-USA"	           "Used"	         33.63
+"Notebooks & Journals"	"Clicked"	     51.94
+"Notebooks & Journals"	"Not Used"	   13.75
+"Notebooks & Journals"	"Used"	       34.31
+"Office"	           "Clicked"	                   50.59
+"Office"	           "Not Used"	                 14.86
+"Office"	           "Used"	                     34.55
+"Waze"	               "Clicked"	                     50.72
+"Waze"                	"Not Used"	                   16.06
+"Waze"                	"Used"	                       33.21
+
+
+2) Were coupons used more than unused?
 
 select
   product_cat
-  , coupon_status
-  , count(coupon_status)
+  , sum(case when coupon_status = 'Used' then 1 else - 1 end) as use_count
 from sales
-where coupon_status = 'Used'
-group by 1,2
-order by 1, 2
+group by 1
+order by 1
+
+"product_cat"	        "use_count"
+++++++++++++++++++++++++++++++++
+"Accessories"	           -80
+"Android"	               -23
+"Apparel"	              -5814
+"Backpacks"	             -35
+"Bags"	                 -632
+"Bottles"	               -104
+"Drinkware"	             -1161
+"Fun"	                    -56
+"Gift Cards"	            -47
+"Google"	                -47
+"Headgear"	             -259
+"Housewares"	            -50
+"Lifestyle"	             -890
+"More Bags"	              -10
+"Nest"	                 -758
+"Nest-Canada"	           -127
+"Nest-USA"	             -4589
+"Notebooks & Journals"	 -235
+"Office"	               -2013
+"Waze"	                 -186
+
+--All categories
