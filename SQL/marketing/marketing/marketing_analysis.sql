aef5ec90af34c984ec10fb98899acd277db4d287
Ross Reisman <Ross.Reisman@gmail.com>
2024-04-07 13:08:00 -0400

    Update marketing_analysis.sql

--- SQL/marketing/marketing/marketing_analysis.sql
+++ SQL/marketing/marketing/marketing_analysis.sql
@@ -20,57 +20,59 @@ Breakdown of products by coupon use
   "Android"	               "Clicked"	     23
   "Android"	               "Not Used"	     10
   "Android"	               "Used"	         10
-  "Apparel"	               "Clicked"	   9223
-  "Apparel"	               "Not Used"	2747
-  "Apparel"	               "Used"	6156
-  "Backpacks"	             "Clicked"	47
-  "Backpacks"	             "Not Used"	15
-  "Backpacks"	             "Used"	27
-  "Bags"	                 "Clicked"	972
-  "Bags"	                 "Not Used"	285
-  "Bags"	                 "Used"	625
-  "Bottles"	               "Clicked"	138
-  "Bottles"	               "Not Used"	48
-  "Bottles"	               "Used"	82
-  "Drinkware"	             "Clicked"	1752
-  "Drinkware"	             "Not Used"	570
-  "Drinkware"	             "Used"	1161
-  "Fun"	                   "Clicked"	83
-  "Fun"	                   "Not Used"	25
-  "Fun"	                   "Used"	52
-  "Gift Cards"	           "Clicked"	81
-  "Gift Cards"	           "Not Used"	22
-  "Gift Cards"	           "Used"	56
-  "Google"	               "Clicked"	51
-  "Google"	               "Not Used"	25
-  "Google"	               "Used"	29
-  "Headgear"	             "Clicked"	401
-  "Headgear"	             "Not Used"	114
-  "Headgear"	             "Used"	256
-  "Housewares"	           "Clicked"	68
-  "Housewares"	           "Not Used"	18
-  "Housewares"	           "Used"	36
-  "Lifestyle"	             "Clicked"	1529
-  "Lifestyle"	             "Not Used"	462
-  "Lifestyle"	             "Used"	1101
-  "More Bags"	             "Clicked"	25
-  "More Bags"	             "Not Used"	3
-  "More Bags"	             "Used"	18
-  "Nest"	                 "Clicked"	1127
-  "Nest"	                 "Not Used"	351
-  "Nest"	                 "Used"	720
-  "Nest-Canada"	           "Clicked"	175
-  "Nest-Canada"	           "Not Used"	47
-  "Nest-Canada"	           "Used"	95
-  "Nest-USA"	             "Clicked"	7141
-  "Nest-USA"	             "Not Used"	2160
-  "Nest-USA"	             "Used"	4712
-  "Notebooks & Journals"	 "Clicked"	389
-  "Notebooks & Journals"	 "Not Used"	103
-  "Notebooks & Journals"	 "Used"	257
-  "Office"	               "Clicked"	3295
-  "Office"	               "Not Used"	968
-  "Office"	               "Used"	2250
-  "Waze"	                 "Clicked"	281
-  "Waze"	                 "Not Used"	89
-  "Waze"	                 "Used"	184
+  "Apparel"	               "Clicked"	    9223
+  "Apparel"	               "Not Used"	    2747
+  "Apparel"	               "Used"	        6156
+  "Backpacks"	             "Clicked"	     47
+  "Backpacks"	             "Not Used"	     15
+  "Backpacks"	             "Used"	         27
+  "Bags"	                 "Clicked"	    972
+  "Bags"	                 "Not Used"	    285
+  "Bags"	                 "Used"	        625
+  "Bottles"	               "Clicked"	    138
+  "Bottles"	               "Not Used"	     48
+  "Bottles"	               "Used"	         82
+  "Drinkware"	             "Clicked"	    1752
+  "Drinkware"	             "Not Used"	     570
+  "Drinkware"	             "Used"	        1161
+  "Fun"	                   "Clicked"	     83
+  "Fun"	                   "Not Used"	     25
+  "Fun"	                   "Used"	         52
+  "Gift Cards"	           "Clicked"	     81
+  "Gift Cards"	           "Not Used"	     22
+  "Gift Cards"	           "Used"	         56
+  "Google"	               "Clicked"	     51
+  "Google"	               "Not Used"	     25
+  "Google"	               "Used"	         29
+  "Headgear"	             "Clicked"	     401
+  "Headgear"	             "Not Used"	     114
+  "Headgear"	             "Used"	         256
+  "Housewares"	           "Clicked"	     68
+  "Housewares"	           "Not Used"	     18
+  "Housewares"	           "Used"	         36
+  "Lifestyle"	             "Clicked"	    1529
+  "Lifestyle"	             "Not Used"	     462
+  "Lifestyle"	             "Used"	        1101
+  "More Bags"	             "Clicked"	     25
+  "More Bags"	             "Not Used"	      3
+  "More Bags"	             "Used"	         18
+  "Nest"	                 "Clicked"	    1127
+  "Nest"	                 "Not Used"	     351
+  "Nest"	                 "Used"	         720
+  "Nest-Canada"	           "Clicked"	     175
+  "Nest-Canada"	           "Not Used"	     47
+  "Nest-Canada"	           "Used"	         95
+  "Nest-USA"	             "Clicked"	    7141
+  "Nest-USA"	             "Not Used"	    2160
+  "Nest-USA"	             "Used"	        4712
+  "Notebooks & Journals"	 "Clicked"	     389
+  "Notebooks & Journals"	 "Not Used"	     103
+  "Notebooks & Journals"	 "Used"	         257
+  "Office"	               "Clicked"	    3295
+  "Office"	               "Not Used"	     968
+  "Office"	               "Used"	        2250
+  "Waze"	                 "Clicked"	     281
+  "Waze"	                 "Not Used"	     89
+  "Waze"	                 "Used"	         184
+
+  
