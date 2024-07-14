d8018bd9a1305fc75ac3fe1417a273b7560abcf8
Ross Reisman <Ross.Reisman@gmail.com>
2024-07-14 11:15:14 -0400

    Update marketing_analysis.sql

--- SQL/marketing/marketing/marketing_analysis.sql
+++ SQL/marketing/marketing/marketing_analysis.sql
@@ -133,25 +133,25 @@ order by 1, 2
   "Lifestyle"	           "Used"	      1101	       3092
   "More Bags"	           "Clicked"	        25	         46
   "More Bags"	           "Not Used"	         3	         46
-  "More Bags"	           "Used"	         18	         46
-  "Nest"	           "Clicked"	       1127	       2198
-  "Nest"	           "Not Used"	        351	       2198
-  "Nest"	           "Used"	        720	       2198
-  "Nest-Canada"	           "Clicked"	         175	        317
-  "Nest-Canada"	           "Not Used"	          47	        317
-  "Nest-Canada"	           "Used"	         95	        317
-  "Nest-USA"	           "Clicked"	        7141	      14013
-  "Nest-USA"	           "Not Used"	        2160	      14013
-  "Nest-USA"	           "Used"	        4712	      14013
-  "Notebooks & Journals"   "Clicked"	         389	        749
-  "Notebooks & Journals"   "Not Used"	         103	        749
-  "Notebooks & Journals"   "Used"	         257	        749
-  "Office"	           "Clicked"	        3295	       6513
-  "Office"	           "Not Used"	         968	       6513
-  "Office"                 "Used"	       2250	       6513
-  "Waze"	           "Clicked"	         281	        554
-  "Waze"	           "Not Used"	          89	        554
-  "Waze"	           "Used"	         184       	  554
+  "More Bags"	           "Used"	        18	         46
+  "Nest"	           "Clicked"	      1127	       2198
+  "Nest"	           "Not Used"	       351	       2198
+  "Nest"	           "Used"	       720	       2198
+  "Nest-Canada"	           "Clicked"	        175	        317
+  "Nest-Canada"	           "Not Used"	         47	        317
+  "Nest-Canada"	           "Used"	        95	        317
+  "Nest-USA"	           "Clicked"	       7141	      14013
+  "Nest-USA"	           "Not Used"	       2160	      14013
+  "Nest-USA"	           "Used"	       4712	      14013
+  "Notebooks & Journals"   "Clicked"	        389	        749
+  "Notebooks & Journals"   "Not Used"	        103	        749
+  "Notebooks & Journals"   "Used"	        257	        749
+  "Office"	           "Clicked"	       3295	       6513
+  "Office"	           "Not Used"	        968	       6513
+  "Office"                 "Used"	      2250	       6513
+  "Waze"	           "Clicked"	        281	        554
+  "Waze"	           "Not Used"	         89	        554
+  "Waze"	           "Used"	        184       	  554
 
 /*
 Our results here include:
